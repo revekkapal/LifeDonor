2db9b76dc878732a913127439ce0feefffea7231
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2015-06-13 22:27:03 +0300

    LoginPresenterImpl skeleton

--- app/src/main/java/com/iliasfotopoulos/lifedonor/Presenter/Implementation/LoginPresenterImpl.java
+++ app/src/main/java/com/iliasfotopoulos/lifedonor/Presenter/Implementation/LoginPresenterImpl.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 
 import com.iliasfotopoulos.lifedonor.Presenter.LoginPresenter;
 import com.iliasfotopoulos.lifedonor.View.LoginView;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 /**
  * Created by ilias on 13/6/2015.
@@ -12,13 +15,33 @@ public class LoginPresenterImpl implements LoginPresenter
 {
     private LoginView view;
 
-    public void setView(LoginView loginview)
+    public LoginPresenterImpl(LoginView loginView)
     {
-        this.view = loginview;
+        this.view = loginView;
     }
 
     public void validateCredentials(String username, String password)
     {
+        view.showProgressBar();
+        // I could have used my User class that extends ParseUser to be decoupled from Parse
+        ParseUser.logInInBackground(username, password, new LogInCallback()
+        {
+            public void done(ParseUser user, ParseException e)
+            {
+                if (user != null)
+                {
+                    // Hooray! The user is logged in.
+                }
+                else
+                {
+                    // Signup failed. Look at the ParseException to see what happened.
+                }
+            }
+        });
+    }
 
+    public void onSuccess()
+    {
+        view.navigateToHome();
     }
 }
